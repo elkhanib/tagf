140765193dcccc410534812a9177a3e2f59958cb
Elkhan Ibrahimov <elkhan.s.ibrahimov@gmail.com>
2020-05-28 00:19:00 +0400

    merge util.go file to another util.go file in common package. also remove util package.

--- common/util.go
+++ common/util.go
@@ -16,7 +16,13 @@ limitations under the License.
 
 package common
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"path"
+	"strings"
+	"time"
+)
 
 // Split breaks a string down into a list of substrings using separator
 func Split(str string, sp string) []string {
@@ -24,4 +30,51 @@ func Split(str string, sp string) []string {
 		return string(c) == sp
 	}
 	return strings.FieldsFunc(str, f)
+}
+
+// AbsolutePath gets absolute path of provided file
+func AbsolutePath(p string) (string, error) {
+	if path.IsAbs(p) {
+		return p, nil
+	}
+	currentDir, err := os.Getwd()
+
+	if err != nil {
+		return "", err
+	}
+	return path.Join(currentDir, path.Clean(p)), nil
+}
+
+func FmtStringSlice(str []string, separator string) string {
+	if len(str) == 1 {
+		return str[0]
+	}
+
+	var formatted string
+	for i, f := range str {
+		if i == len(str)-1 {
+			formatted = formatted + f
+		} else {
+			formatted = formatted + f + separator
+		}
+
+	}
+	return formatted
+}
+
+func ExistsInSlice(s *[]string, elem string) bool {
+	for _, e := range *s {
+		if e == elem {
+			return true
+		}
+	}
+
+	return false
+}
+
+func Elapsed(action string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", action, time.Since(start))
+	}
 }
